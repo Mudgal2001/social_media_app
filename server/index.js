0d98d19dd493bc158c746e98d28e9c579d903074
Yash Mudgal <96115337+Mudgal2001@users.noreply.github.com>
2023-04-04 23:56:39 +0530

    user routes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@
  import morgan from "morgan";
  import path from "path";
  import { fileURLToPath } from "url";
+ import authRoutes from "./routes/auth.js";
  import {register} from "./controllers/auth.js"
  /* Configuation */
  const __filename = fileURLToPath(import.meta.url);
@@ -36,6 +37,9 @@
 
  // Routes wiht Files
  app.post("auth/register",upload.single("picutre"),register);
+
+ //Routes
+ app.use("/auth",authRoutes);
 
  // Mogoose setup
 
