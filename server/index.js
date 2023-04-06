6da6c6e0d2dc9b5c81552299903467cea397d47d
Yash Mudgal <96115337+Mudgal2001@users.noreply.github.com>
2023-04-06 23:40:52 +0530

    user routes done

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@
  import path from "path";
  import { fileURLToPath } from "url";
  import authRoutes from "./routes/auth.js";
+ import userRoutes from "./routes/users.js";
  import {register} from "./controllers/auth.js"
  /* Configuation */
  const __filename = fileURLToPath(import.meta.url);
@@ -40,6 +41,7 @@
 
  //Routes
  app.use("/auth",authRoutes);
+ app.use("/user",userRoutes);
 
  // Mogoose setup
 
